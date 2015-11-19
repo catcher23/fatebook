de8c47547b9bf27f2de9ad85c8e78de59e19cd3f
catcher1 <danlau168@gmail.com>
2015-11-19 11:22:51 -0800

    Fix tracking

--- app/assets/javascripts/components/humans/acontent_header.js.jsx
+++ app/assets/javascripts/components/humans/acontent_header.js.jsx
@@ -12,47 +12,47 @@ var bHumanContentHeader = React.createClass({
     var track = $.extend({}, this.state, { tracker_id: CURRENT_USER_ID, trackee_id: human.id }
     );
 
-    if (window.trackStatus === "Track") {
-      window.trackStatus = "Tracking";
-
-          this.disabledStatus = true;
+    if (trackStatus === "Track") {
+      trackStatus = "Tracking";
+          disabledStatus = true;
       ApiUtil.createTrack(track);
     } else {
-      window.trackStatus = "Untracking";
-          this.disabledStatus = true;
+      trackStatus = "Untracking";
+          disabledStatus = true;
       ApiUtil.destroyTrack(track);
     }
       this.navigateToHumanShow();
     },
 
   render: function () {
-
-  if (window.trackStatus !== 'Tracking' && window.trackStatus !== 'Untracking') {
+    trackStatus = '';
     if (human.tracks.length === 0) {
-      window.trackStatus = 'Track';
+      trackStatus = 'Track';
     } else {
       for (var z = 0; z < human.tracks.length; z++) {
         if (human.tracks[z].tracker_id !== CURRENT_USER_ID) {
-          window.trackStatus = 'Track';
+          trackStatus = 'Track';
         }
         else {
-          window.trackStatus = 'Untrack';
+          trackStatus = 'Untrack';
+          break;
         }
       }
     }
-  }
 
-    if (window.trackStatus == 'Tracking' || window.trackStatus == 'Untracking') {
-      this.disabledStatus = true;
 
+    disabledStatus = '';
+    if (trackStatus == 'Tracking' || trackStatus == 'Untracking') {
+      disabledStatus = true;
     } else {
-      this.disabledStatus = false;
+      disabledStatus = false;
     }
+
     return(
       <header className="content-header2">
 
         <h1>{human.fname+' '+human.lname}</h1>
-        <button type='button' disabled = {this.disabledStatus} className="content-header-add-friend" onClick={this.handleTrackClick}>{window.trackStatus}</button>
+        <button type='button' disabled = {disabledStatus} className="content-header-add-friend" onClick={this.handleTrackClick}>{trackStatus}</button>
       </header>
     );
   }
