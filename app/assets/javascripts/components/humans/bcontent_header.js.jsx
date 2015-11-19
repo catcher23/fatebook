ed11778ef6c3867eb67da9295e718a6f970111af
catcher1 <danlau168@gmail.com>
2015-11-19 11:35:06 -0800

    Fix tracking

---
+++ app/assets/javascripts/components/humans/bcontent_header.js.jsx
@@ -0,0 +1,63 @@
+var aHumanContentHeader = React.createClass({
+  mixins: [ReactRouter.History],
+
+  navigateToHumanShow: function () {
+    var humanUrl = "/humans/" + human.id;
+    this.history.pushState(null, humanUrl, {});
+
+  },
+  handleTrackClick: function () {
+    event.preventDefault();
+    flag = false;
+    var track = $.extend({}, this.state, { tracker_id: CURRENT_USER_ID, trackee_id: human.id }
+    );
+
+    if (window.trackStatus === "Track") {
+      window.trackStatus = "Tracking";
+
+          this.disabledStatus = true;
+      if (ApiUtil.createTrack(track)) {
+      window.trackStatus = 'Untrack';
+      }
+    } else {
+      window.trackStatus = "Untracking";
+          this.disabledStatus = true;
+      if (ApiUtil.destroyTrack(track)) {
+        window.trackStatus = 'Track';
+        }
+    }
+      this.navigateToHumanShow();
+    },
+
+  render: function () {
+
+  if (window.trackStatus !== 'Tracking' && window.trackStatus !== 'Untracking') {
+    if (human.tracks.length === 0) {
+      window.trackStatus = 'Track';
+    } else {
+      for (var z = 0; z < human.tracks.length; z++) {
+        if (human.tracks[z].tracker_id !== CURRENT_USER_ID) {
+          window.trackStatus = 'Track';
+        }
+        else {
+          window.trackStatus = 'Untrack';
+        }
+      }
+    }
+  }
+
+    if (window.trackStatus == 'Tracking' || window.trackStatus == 'Untracking') {
+      this.disabledStatus = true;
+
+    } else {
+      this.disabledStatus = false;
+    }
+    return(
+      <header className="content-header2">
+
+        <h1>{human.fname+' '+human.lname}</h1>
+        <button type='button' disabled = {this.disabledStatus} className="content-header-add-friend" onClick={this.handleTrackClick}>{window.trackStatus}</button>
+      </header>
+    );
+  }
+});
